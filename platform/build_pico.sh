34857863efca81e67b7bd40d2937f2c7f4099f6f
Abhi Gundrala <gundralaa@berkeley.edu>
2023-05-22 15:08:21 -0700

    display lib and build modifications

--- platform/build_pico.sh
+++ platform/build_pico.sh
@@ -10,8 +10,17 @@ cp "../../platform/lf_pico_support.c" "${LF_SOURCE_GEN_DIRECTORY}/core/platform"
 cp "../../platform/lf_pico_support.h" "${LF_SOURCE_GEN_DIRECTORY}/include/core/platform"
 cp "../../platform/platform.h" "${LF_SOURCE_GEN_DIRECTORY}/include/core"
 
+# vscode settings
+mkdir .vscode
+cp "../../platform/launch.json" "${LF_SOURCE_GEN_DIRECTORY}/.vscode"
+
+#sudo service udev restart && sudo udevadm trigger
+
 mkdir build
 cd build
 cmake ..
 cmake --build .
+code ../
+#cmake -b build
+#cmake --build .
 
